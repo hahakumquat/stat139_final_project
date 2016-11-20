1f22cdf06c1ffffb8c7e252109724a72af00d16e
Michael Ge <michaelge@college.harvard.edu>
2016-11-19 23:42:42 -0500

    proposal

---
+++ proposal/auto/proposal.el
@@ -0,0 +1,17 @@
+(TeX-add-style-hook
+ "proposal"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "12pt")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("geometry" "margin=1in")))
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art12"
+    "graphicx"
+    "amssymb"
+    "mdframed"
+    "geometry"))
+ :latex)
+
